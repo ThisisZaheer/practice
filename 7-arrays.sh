f32536221312362e8a5fdc115fea7a0bb6b6a882
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-26 09:09:58 +0530

    practice

--- 7-arrays.sh
+++ 7-arrays.sh
@@ -5,4 +5,4 @@ Cadillac=("black" "white" "grey")
 echo "1st Color:${Cadillac[0]}"
 echo "2nd Color:${Cadillac[1]}"
 echo "3rd Color:${Cadillac[2]}"
-echo All Colors:${Cadillac[@]}
+echo "All Colors:${Cadillac[@]}"
