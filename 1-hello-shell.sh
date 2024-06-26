73303cfe55e1b84f0000381b00cba46cb2f7273e
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-26 07:47:47 +0530

    practice

--- 1-hello-shell.sh
+++ 1-hello-shell.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-shaik:: Assalamu alaikum mohammed, how are you?
-mohammed:: Valaikum assalam shaik, i am good, how are you
-shaik:: i am also good, what are you doing now-a-days
-mohammed:: i am upskilling myself through DevOps course
-shaik:: Ohh! that's great
+echo "hey, hi i am learning Devops with AWS Course" 
