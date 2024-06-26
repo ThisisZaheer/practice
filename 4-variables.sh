eb9f1f2e6cc36539ca7798f201fc907cb5da1ef0
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-26 08:28:36 +0530

    practice

---
+++ 4-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name1=$1
+name2=$2
+
+echo "${name1}:: Assalamu alaikum ${name2}, how are you?"
+echo "${name2}:: Valaikum assalam ${name1}, i am good, how are you?"
+echo "${name1}:: i am also good, i heard that you are learning DevOps Course"
+echo "${name2}:: yes i am learning DevOps with AWS Course to enhance my skills"
