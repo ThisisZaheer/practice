ec2e6bb9ec94fdb918ada3b57a5c12f8c3635287
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-26 07:52:54 +0530

    practice

---
+++ 2-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+shaik:: Assalamu alaikum syed, how are you?
+syed:: Valaikum assalam shaik, i am fine, how is your work?
+shaik:: it's good syed, i am started learning DevOps with AWS Course
