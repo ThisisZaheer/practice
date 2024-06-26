db655c5c36dead9288ff94283ce27cc902c7ed64
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-26 11:59:30 +0530

    datatypes

---
+++ 2.1-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name1=Dad
+name2=Son
+
+echo "${name1}:: Assalamu alaikum ${name2}, how are you?"
+echo "${name2}:: Valaikum assalam ${name1}, i am good, how are you?"
+echo "${name1}:: i am also good, i heard that you are learning DevOps Course"
+echo "${name2}:: yes i am learning DevOps with AWS Course to enhance my skills"
--- 3-variables.sh
+++ 3-variables.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-name1=Dad
-name2=Son
+echo "Please enter your email:"
 
-echo "${name1}:: Assalamu alaikum ${name2}, how are you?"
-echo "${name2}:: Valaikum assalam ${name1}, i am good, how are you?"
-echo "${name1}:: i am also good, i heard that you are learning DevOps Course"
-echo "${name2}:: yes i am learning DevOps with AWS Course to enhance my skills"
+read MAIL
+
+echo "Please enter you passkey"
+
+read PASSWORD
+
+echo "email:${MAIL} , passkey:${PASSWORD}"
