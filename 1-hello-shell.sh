7487c4d3057a3736270d173d0c87756e174cbe14
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-26 07:37:37 +0530

    practice

---
+++ 1-hello-shell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+shaik:: Assalamu alaikum mohammed, how are you?
+mohammed: Valaikum assalam shaik, i am fine, hope you are also good, how's the life?
+shaik: good, i am upgrading to DevOps course in these days
+mohammed: ohh!! that's great.
