2cb198b2a13b134f2522772085ac0de7e50327b9
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-26 08:23:55 +0530

    practice

---
+++ 3-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name1=shaik
+name2=mohammed
+
+echo "shaik:: Assalamu alaikum mohammed, how are you?"
+echo "mohammed:: Valaikum assalam shaik, i am good, how are you?"
+echo "shaik:: i am also good, i heard that you are learning DevOps Course"
+echo "mohammed:: yes i am learning DevOps with AWS Course to enhance my skills"
