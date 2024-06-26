506e43b004da430bbd20132b6783a79c25c3b0cc
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-26 12:21:23 +0530

    datatypes

--- 6-special-vars.sh
+++ 6-special-vars.sh
@@ -7,3 +7,9 @@ echo "No.of Variables:$#"
 echo "Present Working Directory:$PWD"
 
 echo "Script-name:$0"
+
+echo "Home-Directory of current user:$HOME"
+
+echo "Hostname:$HOSTNAME"
+
+echo "Which User running the script:$USER"
