191453be72c3e9494ce7c5c72bded0369554bc6f
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-27 20:09:52 +0530

    functions

--- 9-funtions.sh
+++ 9-funtions.sh
@@ -2,10 +2,22 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d . -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is:$R Failed $N"
+        exit 1
+    else 
+        echo -e "$2 is:$G Success $N"
+    fi
+}
 
 if [ $USERID -ne 0 ]
 then
@@ -14,3 +26,9 @@ then
 else 
    echo -e "$G You are a Super-User $N"
 fi
+
+dnf install mysql -y
+validate $? "Installing Mysql Server"
+
+dnf install git -y
+validate $? "Installing git"
