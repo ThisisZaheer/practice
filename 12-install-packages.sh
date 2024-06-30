6deea926f7e31e67f99d24c5e33f9ffa30350d73
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-30 19:53:28 +0530

    loops

---
+++ 12-install-packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+SCRIPT_NAME=$(echo $0 | cut -d . -f1)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$C Please run this script with root acess $N"
+    exit 1
+else
+    echo -e "$Y You are a Super-User $N"
+fi
